4192d39a8f9fa72595d82d749539db3a2a5a1d87
Dylan Loader <dylanloader@gmail.com>
2022-10-29 23:19:33 -0600

    All components for part 1 of tutorial complete

--- ArjanCodes/getting-started-with-dash/main.py
+++ ArjanCodes/getting-started-with-dash/main.py
@@ -1,8 +1,9 @@
 from dash import Dash, html
+from dash_bootstrap_components.themes import BOOTSTRAP
 from src.components.layout import create_layout
 
 def main() -> None:
-    app = Dash()
+    app = Dash(external_stylesheets=[BOOTSTRAP])
     app.title = "Medal Dashboard"
     app.layout = create_layout(app) # Set placeholder layout
     app.run()
