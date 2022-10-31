c78cf5d82403ea5e2b3ae63781ff5986df7852cd
Dylan Loader <dylanloader@gmail.com>
2022-10-30 19:18:09 -0600

    Refactored for part 2 of the tutorial

--- ArjanCodes/getting-started-with-dash/src/components/layout.py
+++ ArjanCodes/getting-started-with-dash/src/components/layout.py
@@ -1,18 +1,28 @@
+import pandas as pd
 from dash import Dash, html
-from . import nation_dropdown
-from . import bar_chart
+from src.components import (
+    year_dropdown,
+    bar_chart,
+    month_dropdown,
+    category_dropdown,
+    pie_chart
+)
 
-def create_layout(app: Dash) -> html.Div:
+def create_layout(app: Dash, data:pd.DataFrame) -> html.Div:
     return html.Div(
         className = "app-div",
         children = [
             html.H1(app.title),
             html.Hr(),
-            html.Div(className="dropdown-container",
-                     children=[
-                         nation_dropdown.render(app)
-                     ]),
-            bar_chart.render(app)
-            
-                    ]
-    )
+            html.Div(
+                    className="dropdown-container",
+                    children=[
+                        year_dropdown.render(app,data),
+                        month_dropdown.render(app,data),
+                        category_dropdown.render(app,data),
+                        ],
+                    ),
+                    bar_chart.render(app,data),
+                    pie_chart.render(app,data),
+                ],
+        )
