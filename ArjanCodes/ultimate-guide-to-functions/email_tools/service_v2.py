8950485dadc2526b2772f9779a36dfc6342a8ab0
Dylan Loader <dylanloader@gmail.com>
2023-05-29 15:23:41 -0600

    Added new arjan class tutorial

---
+++ ArjanCodes/ultimate-guide-to-functions/email_tools/service_v2.py
@@ -0,0 +1,22 @@
+from email.message import EmailMessage
+from smtplib import SMTP_SSL
+
+def create_email_message(to_email:str, subject:str, body:str) -> EmailMessage:
+    msg = EmailMessage()
+    msg.set_content(body)
+    msg['Subject'] = subject
+    msg["To"] = to_email
+    return msg
+
+def send_email(
+    smtp_server: str, 
+    port: int, 
+    email: str, 
+    password: str, 
+    to_email:str, 
+    subject: str, 
+    body: str,
+) -> None:
+    msg = create_email_message(to_email, subject, body)
+    with SMTP_SSL(smtp_server, port) as server:
+        print("Email sent successfully!")
